765629d614013bdb83af848041380403ad4b1d9c
lgrock007 <marshmelloxoxo1986@gmail.com>
2023-07-08 21:42:22 +0530

    new

---
+++ 16_fibonacci_series_upto_nth_term.py
@@ -0,0 +1,10 @@
+n = int(input('Enter the number: '))
+first = 0 
+second = 1
+print('Fibonacci Series:')
+for i in range(n):
+    print(first,end=' ')
+    temp = first
+    first = second
+    second += temp
+     
