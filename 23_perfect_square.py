21a1e2a474d11520cf4fd26f1fe938579216ca59
lgrock007 <marshmelloxoxo1986@gmail.com>
2023-07-27 18:14:11 +0530

    perfect square

--- 23_perfect_square.py
+++ 23_perfect_square.py
@@ -1,14 +1,14 @@
 # PERFECT SQUARE
 
 num = int(input('Enter the number: '))
-X = 0
+flag = 0
 
 for i in range(1,num):
     if i * i == num :
-        X = 1
+        flag = 1
         break
 
-if x == 1 :
+if flag == 1 :
     print('it\'s a perfect square')
 else:
     print('not a perfect square')
